394c230448d1af9ebc90416c15a43fcabd06a62d
wahyuaditya0 <wah.aditya0@gmail.com>
2021-01-28 16:38:06 +0700

    updated

---
+++ jq.js
@@ -0,0 +1,13 @@
+// $(".nav-link").on("click", function () {
+//   // ambil isi href
+
+//   var href = $(this).attr("href");
+
+//   //tangkap elemen
+//   var elemenHref = $(href);
+//   // console.log(elemenHref.offset().top);
+
+//   $("body").scrollTop(elemenHref.offset().top - 100);
+// });
+
+$("href").easeInOutExpo;
